b1420703c588792007e351068afa4d6c848653ce
espinarjoel@gmail.com <espinarjoel@gmail.com>
2021-07-26 02:00:29 +0800

    commit 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Games Reid!'));
+app.get('/', (req, res) => res.send('Hello Games Reid ulit!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
